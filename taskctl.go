5c8baf91a0a0ef85965381857e4e219118b2c5c6
Dmitriy Kurochkin <dm.v.kurochkin@gmail.com>
2016-08-28 18:43:35 +0200

    Write task status to file

--- taskctl.go
+++ taskctl.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"os/user"
 )
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func getPath(task string) string {
 	usr, _ := user.Current()
@@ -11,7 +17,21 @@ func getPath(task string) string {
 	return usr.HomeDir + "/.taskctl/tasks/" + task + ".txt"
 }
 
+func writeStatus(task string, status string) {
+	filename := getPath(task)
+	data := status + "|" + "time" + "\n"
+
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0644)
+	check(err)
+
+	defer f.Close()
+
+	_, err = f.WriteString(data)
+	check(err)
+}
+
 func main() {
-	path := getPath("ASS-0001")
-	fmt.Println(path)
+	task := "ASS-2103"
+	status := "START"
+	writeStatus(task, status)
 }
