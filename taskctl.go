9a12d0be66d37eb5ef341b9433e29bd756a459b9
Dmitriy Kurochkin <dmitriy.kurochkin@avira.com>
2016-08-30 17:45:13 +0200

    Added "list" command prototype

--- taskctl.go
+++ taskctl.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/d-kurochkin/taskctl/src/tasks"
+	"github.com/olekukonko/tablewriter"
 	"github.com/urfave/cli"
 )
 
@@ -29,6 +30,29 @@ func main() {
 				task := c.Args().First()
 				tasks.WriteStatus(task, "STOP")
 				fmt.Println("stopped task: ", task)
+				return nil
+			},
+		},
+		{
+			Name:  "list",
+			Usage: "list all tasks",
+			Action: func(c *cli.Context) error {
+				data := [][]string{
+					[]string{"TAR-100", "10 h"},
+					[]string{"TAR-101", "2 h"},
+					[]string{"TAR-103", "4 h"},
+					[]string{"TAR-99", "5 h"},
+				}
+
+				table := tablewriter.NewWriter(os.Stdout)
+				table.SetRowLine(true)
+				table.SetRowSeparator("-")
+				table.SetHeader([]string{"Task", "Logged time"})
+
+				for _, v := range data {
+					table.Append(v)
+				}
+				table.Render()
 				return nil
 			},
 		},
