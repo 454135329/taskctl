137bbcafbc6db4b1166428a8547c32932463b53d
Dmitriy Kurochkin <dm.v.kurochkin@gmail.com>
2016-09-10 23:01:57 +0200

    #1 Implemented task marshalling

--- src/tasks/tasks.go
+++ src/tasks/tasks.go
@@ -1,6 +1,9 @@
 package tasks
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/user"
 	"strconv"
@@ -29,32 +32,36 @@ var messages = map[string]string{
 
 // OpenTask reads existing task or create new one
 func OpenTask(name string) Task {
+	path := getTaskPath(name)
 
-	return Task{Name: "TSK-001", Status: "start"}
+	if fileExists(path) {
+		fmt.Println("exists")
+	}
+
+	return Task{Name: name, Status: "todo"}
 }
 
 // Close writes changes to file
-func (task Task) Close() {
+func (task *Task) Close() {
+	path := getTaskPath(task.Name)
+	data, _ := json.Marshal(task)
 
+	_ = ioutil.WriteFile(path, data, 0644)
 }
 
 // Start changes status to in progress
 func (task *Task) Start() {
-
+	task.Status = "start"
 }
 
 // Stop changes status to stopped
 func (task *Task) Stop() {
-
+	task.Status = "stop"
 }
 
 // Done changes status to done
 func (task *Task) Done() {
-
-}
-
-func (task Task) getTaskPath() string {
-	return getTasksDir() + "/" + task.Name + ".txt"
+	task.Status = "done"
 }
 
 func check(err error) {
@@ -88,6 +95,10 @@ func getTasksDir() string {
 	usr, _ := user.Current()
 
 	return usr.HomeDir + "/.taskctl/tasks"
+}
+
+func getTaskPath(name string) string {
+	return getTasksDir() + "/" + name + ".txt"
 }
 
 func formatDuration(seconds int) string {
